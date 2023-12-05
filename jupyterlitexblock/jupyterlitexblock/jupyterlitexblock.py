301c3a8c569eb7acf84f1034181bd31dab41a007
“Rimsha <rimsha.zaib@arbisoft.com>
2023-12-05 18:41:10 +0500

    fix: resolve pop-up duplication issue

--- jupyterlitexblock/jupyterlitexblock/jupyterlitexblock.py
+++ jupyterlitexblock/jupyterlitexblock/jupyterlitexblock.py
@@ -1,4 +1,4 @@
-"""TO-DO: Write a description of what this XBlock is."""
+"""XBlock for embedding JupyterLite in Open edX."""
 
 import pkg_resources,os
 from web_fragments.fragment import Fragment
@@ -17,7 +17,7 @@ from webob import Response
 
 log = logging.getLogger(__name__)
 
-
+@XBlock.wants("settings")
 class JupterLiteXBlock(XBlock):
     """
        EdX XBlock for embedding JupyterLite, allowing learners to interact with Jupyter notebooks.
@@ -29,7 +29,7 @@ class JupterLiteXBlock(XBlock):
         display_name="JupyterLite Service URL",
         help="The URL of the JupyterLite service",
         scope=Scope.settings,
-        default="http://localhost:9500/lab/"
+        default="http://jupyterlite.local.overhang.io:9500/lab/index.html"
     )
     default_notebook = String(
         display_name="Default Notebook",
@@ -37,12 +37,41 @@ class JupterLiteXBlock(XBlock):
         help="The default notebook for the JupyterLite service",
         default=""
     )
+    viewed_by_learner = String(
+        display_name="Saved Notebook URLs",
+        default="",
+        scope=Scope.user_state,
+        help="List of notebook URLs saved by the learner."
+    )
     display_name = String(
         display_name=("JupyterLite "),
         help=("Display name for this module"),
         default="JupyterLite Notebook",
         scope=Scope.settings
     )
+    
+    def notebook_location(self):
+        """
+        Notebooks will be stored in a media folder with this name
+        """
+        return self.xblock_settings.get("LOCATION", "jupyterlite_notebooks")
+
+    @property
+    def xblock_settings(self):
+        """
+        Return a dict of settings associated to this XBlock.
+        """
+        settings_service = self.runtime.service(self, "settings") or {}
+        if not settings_service:
+            return {}
+        return settings_service.get_settings_bucket(self)
+
+    @property
+    def folder_base_path(self):
+        """
+        Path to the folder where notebooks will be saved.
+        """
+        return os.path.join(self.notebook_location(), self.location.block_id)
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -52,13 +81,24 @@ class JupterLiteXBlock(XBlock):
     def render_template(self, template_path, context):
         template_str = self.resource_string(template_path)
         template = Template(template_str)
-        rendered_template = template.render(Context({'context': context}))
+        rendered_template = template.render(Context(context))
         return rendered_template
     
     def student_view(self, context=None):
         file_name = self.default_notebook
         base_url = self.jupyterlite_url
-        jupyterlite_iframe = '<iframe src="{}?fromURL={}" width="100%" height="600px" style="border: none;"></iframe>'.format(base_url, file_name)
+        notebook_url = '{}?fromURL={}'.format(base_url, file_name)
+        if notebook_url in self.viewed_by_learner.split(','):
+            # If notebook URL is already in the string, set fromURL to an empty string
+            notebook_url = '{}?fromURL='.format(base_url)
+        else:
+            if self.viewed_by_learner:
+                self.viewed_by_learner += ',' + notebook_url
+            else:
+                self.viewed_by_learner = notebook_url
+            self.save()
+
+        jupyterlite_iframe = '<iframe src="{}" width="100%" height="600px" style="border: none;"></iframe>'.format(notebook_url)
         html = self.resource_string("static/html/jupyterlitexblock.html").format(jupyterlite_iframe=jupyterlite_iframe, self=self)
         frag = Fragment(html)
         frag.initialize_js('JupterLiteXBlock')
@@ -71,9 +111,10 @@ class JupterLiteXBlock(XBlock):
         )
     
     def studio_view(self, context=None):
+        notebook_name = os.path.basename(self.default_notebook) if self.default_notebook else ""
         studio_context = {
-            "jupyterlite_url": self.fields["jupyterlite_url"],
-            "default_notebook": self.fields["default_notebook"]
+            "jupyterlite_url": self.jupyterlite_url,
+            "notebook_name": notebook_name,
         } 
         studio_context.update(context or {})
         template = self.render_template("static/html/upload.html", studio_context)
@@ -83,7 +124,7 @@ class JupterLiteXBlock(XBlock):
         return frag
     
     def save_file(self, uploaded_file):
-        path = default_storage.save(f'static/{uploaded_file.name}', ContentFile(uploaded_file.read()))
+        path = default_storage.save(f'{self.folder_base_path}/{uploaded_file.name}', ContentFile(uploaded_file.read()))
         scheme = "https" if settings.HTTPS == "on" else "http"
         root_url = f'{scheme}://{settings.CMS_BASE}'
         tmp_file = os.path.join(settings.MEDIA_ROOT, path)
@@ -95,9 +136,8 @@ class JupterLiteXBlock(XBlock):
         """
         Handle form submission in Studio.
         """
-        get_url = request.params.get("jupyterlite_url",None)
+        self.jupyterlite_url = str(request.params.get("jupyterlite_url", ""))
         notebook = request.params.get("default_notebook").file
-        self.jupyterlite_url = get_url
         self.default_notebook = self.save_file(notebook)
         response = {"result": "success", "errors": []}
         return self.json_response(response)
